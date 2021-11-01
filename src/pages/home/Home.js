d60a88a005927ee731d051974971d5e70ee69f03
Yusuf Isbilir <yusufemirisbilir@gmail.com>
2021-11-01 14:10:12 +0300

    Create recipe list component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,11 @@
+//hooks
 import { useFetch } from "../../hooks/useFetch";
 
-import "./Home.sass";
+//components
+import RecipeList from "../../components/RecipeList";
 
-import React from "react";
+//stylesheets
+import "./Home.sass";
 
 function Create() {
   const { data, isPending, error } = useFetch("http://localhost:3000/recipes");
@@ -11,7 +14,7 @@ function Create() {
     <div className="home">
       {error && <p className="error">{error}</p>}
       {isPending && <p className="loading">Loading...</p>}
-      {data && data.map((recipe) => <h2 key={recipe.id}>{recipe.title}</h2>)}
+      {data && <RecipeList recipes={data} />}
     </div>
   );
 }
