315e7fa553847adaa823464d0e32acf6377c81ad
Yusuf Isbilir <yusufemirisbilir@gmail.com>
2021-11-01 14:24:52 +0300

    Fetch recipe from id.

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,9 +1,22 @@
+import { useParams } from "react-router";
+import { useFetch } from "../../hooks/useFetch";
+
 import "./Recipe.sass";
 
 import React from "react";
 
 function Create() {
-  return <div>Recipe</div>;
+  const { id } = useParams();
+  const url = `http://localhost:3000/recipes/${id}`;
+  const { data: recipe, isPending, error } = useFetch(url);
+
+  return (
+    <div className="recipe">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {recipe && <h1>{recipe.title}</h1>}
+    </div>
+  );
 }
 
 export default Create;
