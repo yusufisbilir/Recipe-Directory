69fc22685bebfbe6b50b9c8760e0ab8d2fdaacdc
Yusuf Isbilir <yusufemirisbilir@gmail.com>
2021-11-01 14:51:49 +0300

    Create Recipe Form.

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,9 +1,59 @@
+import { useState } from "react";
+
 import "./Create.sass";
 
 import React from "react";
 
 function Create() {
-  return <div>Create</div>;
+  const [title, setTitle] = useState("");
+  const [method, setMethod] = useState("");
+  const [cookingTime, setCookingTime] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title, method, cookingTime);
+  };
+
+  return (
+    <div className="create">
+      <h2 className="page-title">Add a New Recipe</h2>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Recipe Title: </span>
+          <input
+            type="text"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Recipe Method: </span>
+          <textarea
+            onChange={(e) => setMethod(e.target.value)}
+            value={method}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Cooking Time: </span>
+          <input
+            type="number"
+            onChange={(e) => setCookingTime(e.target.value)}
+            value={cookingTime}
+            required
+          />
+        </label>
+
+        <button className="btn" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Create;
