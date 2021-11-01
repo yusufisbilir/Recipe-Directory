6be060e7b2a575f08e3056476dafb8b5299caac0
Yusuf Isbilir <yusufemirisbilir@gmail.com>
2021-11-01 13:49:54 +0300

    Fetch data.

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,19 @@
+import { useFetch } from "../../hooks/useFetch";
+
 import "./Home.sass";
 
 import React from "react";
 
 function Create() {
-  return <div>Home</div>;
+  const { data, isPending, error } = useFetch("http://localhost:3000/recipes");
+
+  return (
+    <div className="home">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {data && data.map((recipe) => <h2 key={recipe.id}>{recipe.title}</h2>)}
+    </div>
+  );
 }
 
 export default Create;
