eecc34e17f4ada589264285b3a6f8811e862edd7
Yusuf Isbilir <yusufemirisbilir@gmail.com>
2021-11-01 16:20:34 +0300

    Search recipes

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,9 +1,27 @@
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
+import RecipeList from "../../components/RecipeList";
+
 import "./Search.sass";
 
-import React from "react";
-
 function Create() {
-  return <div>Search</div>;
+  const queryString = useLocation().search;
+  const queryParams = new URLSearchParams(queryString);
+  const query = queryParams.get("q");
+
+  const url = `http://localhost:3000/recipes?q=${query}`;
+  const { error, isPending, data } = useFetch(url);
+
+  return (
+    <div>
+      <h2 className="page-title">Recipes including "{query}"</h2>
+
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {data && <RecipeList recipes={data} />}
+    </div>
+  );
 }
 
 export default Create;
